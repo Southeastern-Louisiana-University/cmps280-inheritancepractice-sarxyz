4931eb6dbdd2e7d29d92a6db6c726e81fffa85fa
sarxyz <sar@selu.edu>
2023-10-04 16:38:08 -0500

    office recreated

---
+++ chapter13/GeoObjTest.java
@@ -0,0 +1,8 @@
+package chapter13;
+
+public class GeoObjTest {
+    public static void main(String args[]){
+        Circle circle = new Circle();
+        System.out.println(circle.toString());
+    }
+}
