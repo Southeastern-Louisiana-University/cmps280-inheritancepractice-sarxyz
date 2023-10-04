7c92cd014bb8d18aa61476d346948fc03593e0ba
sarxyz <sar@selu.edu>
2023-10-04 16:47:03 -0500

    office recreated

--- chapter13/GeoObjTest.java
+++ chapter13/GeoObjTest.java
@@ -4,5 +4,7 @@ public class GeoObjTest {
     public static void main(String args[]){
         Circle circle = new Circle();
         System.out.println(circle.toString());
+        GeometricObject geoObj1 = new Rectangle();
+        System.out.println("printing geoObj1.toString() " + geoObj1.toString());
     }
 }
--- chapter13/Rectangle.java
+++ chapter13/Rectangle.java
@@ -1,4 +1,16 @@
 package chapter13;
 
-public class Rectangle {
+public class Rectangle extends GeometricObject {
+    private double height = 1;
+    private double length = 1;
+
+    @Override /** Return perimeter */
+    public double getPerimeter() {
+        return 2 * (height + length);
+    }
+
+    @Override /** Return perimeter */
+    public double getArea() {
+        return height * length;
+    }
 }
